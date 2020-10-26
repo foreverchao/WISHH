20e904fc70d277c45d4a2837ede33bd54c974993
foreverchao <a0926535949@gmail.com>
2020-10-27 06:14:03 +0800

    SwitchandDoorEvent

---
+++ WISHH/assets/scripts/switch.js
@@ -0,0 +1,59 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        /*afterSwitch : {
+            default: null,
+            type　: cc.SpriteFrame,
+        },
+
+        afterSwitchDoor : {
+            default: null,
+            type　: cc.SpriteFrame,
+        },*/
+
+        afterDoor:{
+            default : null,
+            type : cc.Node,
+        },
+
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    onCollisionEnter(other, self)
+    {
+       this.node.getComponent(cc.Animation).play();
+       this.afterDoor.getComponent(cc.Animation).play();
+       this.afterDoor.getComponent(cc.PhysicsBoxCollider).enabled = false;
+    },
+    // update (dt) {},
+});
